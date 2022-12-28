92c0975d97621557236bef38ecd3146371f8d9ce
jeanmiCORNEC <maelgut@hotmail.fr>
2022-12-28 12:39:26 +0100

    créatoin de l'entité Salle

---
+++ src/Entity/Salle.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SalleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SalleRepository::class)]
+class Salle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $place = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $localisation = null;
+
+    #[ORM\Column]
+    private ?bool $isDisponible = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $observation = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $options = null;
+
+    #[ORM\Column]
+    private ?bool $IsPMR = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPlace(): ?int
+    {
+        return $this->place;
+    }
+
+    public function setPlace(int $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getLocalisation(): ?string
+    {
+        return $this->localisation;
+    }
+
+    public function setLocalisation(string $localisation): self
+    {
+        $this->localisation = $localisation;
+
+        return $this;
+    }
+
+    public function isIsDisponible(): ?bool
+    {
+        return $this->isDisponible;
+    }
+
+    public function setIsDisponible(bool $isDisponible): self
+    {
+        $this->isDisponible = $isDisponible;
+
+        return $this;
+    }
+
+    public function getObservation(): ?string
+    {
+        return $this->observation;
+    }
+
+    public function setObservation(?string $observation): self
+    {
+        $this->observation = $observation;
+
+        return $this;
+    }
+
+    public function getOptions(): ?string
+    {
+        return $this->options;
+    }
+
+    public function setOptions(?string $options): self
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    public function isIsPMR(): ?bool
+    {
+        return $this->IsPMR;
+    }
+
+    public function setIsPMR(bool $IsPMR): self
+    {
+        $this->IsPMR = $IsPMR;
+
+        return $this;
+    }
+}
